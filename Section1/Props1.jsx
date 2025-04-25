252a385904dbf143872b1acfc594ef5d5477bb11
AABITH ROSHAN <130814060+AABITH-ROSHAN@users.noreply.github.com>
2025-04-25 17:29:43 +0530

    Add files via upload

---
+++ Section1/Props1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Props2 from './Props2'
+const props1=() =>{
+  return (
+    <div>
+      <Props2 name="Aabith"/>
+    </div>
+  )
+}
+
+export default props1
---
+++ Section1/Props2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function props2({name}) {
+  return (
+    <div>
+      <h1>`Iam ${name}`</h1>
+    </div>
+  )
+}
+
+export default props2
